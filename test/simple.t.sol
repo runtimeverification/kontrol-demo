468eb2fa85890bcb156eb4fb4449ecf9c8458643
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2023-01-10 15:04:03 +0000

    test/simple: update tests and output

--- test/simple.t.sol
+++ test/simple.t.sol
@@ -11,6 +11,10 @@ contract Examples is Test {
         z = (x * y) / WAD;
     }
 
+    function wdiv(uint x, uint y) internal pure returns (uint z) {
+        z = (x * WAD) / y;
+    }
+
     function test_assert_bool_failing(bool b) public {
         assert(b);
     }
@@ -21,14 +25,22 @@ contract Examples is Test {
         }
     }
 
-    function test_wmul_strictly_increasing(uint a, uint b) public {
+    function test_wmul_increasing_overflow(uint a, uint b) public {
+        uint c = wmul(a, b);
+        assertTrue(a < c && b < c);
+    }
+    // { true #Equals ( ( 115792089237316195423570985008687907853269984665640564039457584007913129639935 /Int VV0_a_3c5818c8 ) ) <Int VV1_b_3c5818c8 }
+    // { true #Equals ( ( maxUInt256 /Int VV0_a_3c5818c8 ) ) <Int VV1_b_3c5818c8 }
+
+    function test_wmul_increasing(uint a, uint b) public {
         if (b <= MAX_INT / a) {
             uint c = wmul(a, b);
             assertTrue(a < c && b < c);
         }
     }
+    // { true #Equals VV0_a_3c5818c8 ==K 0 }
 
-    function test_wmul_increasing(uint a, uint b) public {
+    function test_wmul_increasing_positive(uint a, uint b) public {
         if (0 < a && 0 < b) {
             if (b <= MAX_INT / a) {
                 uint c = wmul(a, b);
@@ -36,12 +48,33 @@ contract Examples is Test {
             }
         }
     }
+    // { true #Equals ( ( ( ( VV0_a_3c5818c8 *Int VV1_b_3c5818c8 ) ) /Int 1000000000000000000 ) ) <=Int VV0_a_3c5818c8 }
 
-    function test_wmul_increasing_2(uint a, uint b) public {
-        if (WAD <= a && WAD <= b) {
+    function test_wmul_increasing_gt_one(uint a, uint b) public {
+        if (WAD < a && WAD < b) {
             if (b <= MAX_INT / a) {
                 uint c = wmul(a, b);
                 assertTrue(a < c && b < c);
+            }
+        }
+    }
+    // #Top
+
+    function test_wmul_wdiv_inverse_underflow(uint a, uint b) public {
+        if (0 < a && 0 < b) {
+            if (b <= MAX_INT / a) {
+                uint c = wdiv(wmul(a, b), b);
+                assertEq(a, c);
+            }
+        }
+    }
+    // { true #Equals maxUInt256 /Word ( ( ( ( VV0_a_3c5818c8 *Int VV1_b_3c5818c8 ) ) /Int 1000000000000000000 ) ) <Int 1000000000000000000 }
+
+    function test_wmul_wdiv_inverse(uint a, uint b) public {
+        if (WAD < a && WAD < b) {
+            if (b <= MAX_INT / a) {
+                uint c = wdiv(wmul(a, b), b);
+                assertEq(a, c);
             }
         }
     }
