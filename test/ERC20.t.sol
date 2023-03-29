bf78bf1f39d3665f258582d3d4590a9766e65d6c
Andrei <andrei.vacaru@runtimeverification.com>
2023-03-29 13:28:50 +0300

    update passing tests

--- test/ERC20.t.sol
+++ test/ERC20.t.sol
@@ -8,6 +8,7 @@ import "forge-std/Test.sol";
 contract ERC20Test is Test, KEVMCheats {
 
     uint8 constant BALANCES_STORAGE_INDEX = 0;
+    uint256 constant ALLOWANCES_STORAGE_INDEX = 1;
     uint256 constant TOTALSUPPLY_STORAGE_INDEX = 2;
     
     function getStorageLocationForKey(address _key, uint8 _index) public pure returns(bytes32) {
@@ -49,11 +50,10 @@ contract ERC20Test is Test, KEVMCheats {
     ****************************/
 
     function testTotalSupply(uint256 amount) public {
-        //kevm.infiniteGas();
+        kevm.infiniteGas();
         ERC20 erc20 = new ERC20("Bucharest Workshop Token", "BWT");
-        //kevm.symbolicStorage(address(erc20));
-        bytes32 storageValue = vm.load(address(erc20), bytes32(TOTALSUPPLY_STORAGE_INDEX));
-        vm.assume(uint256(storageValue) == amount);
+        kevm.symbolicStorage(address(erc20));
+        vm.store(address(erc20), bytes32(TOTALSUPPLY_STORAGE_INDEX), bytes32(amount));
         uint256 totalSupply = erc20.totalSupply();
         assertEq(totalSupply, amount);
     }
@@ -68,8 +68,8 @@ contract ERC20Test is Test, KEVMCheats {
         kevm.infiniteGas();
         ERC20 erc20 = new ERC20("Bucharest Workshop Token", "BWT");
         kevm.symbolicStorage(address(erc20));
-        bytes32 storageLocation = getStorageLocationForKey(addr, BALANCES_STORAGE_INDEX);
-        vm.assume(uint256(vm.load(address(erc20), storageLocation)) == amount);
+        bytes32 storageLocation = getStorageLocationForKey(addr, BALANCES_STORAGE_INDEX); //compute the storage location of _balances[addr]
+        vm.store(address(erc20), storageLocation, bytes32(amount));
         uint256 balance = erc20.balanceOf(addr);
         assertEq(balance, amount);
     }
@@ -80,13 +80,13 @@ contract ERC20Test is Test, KEVMCheats {
     *
     ****************************/
 
-    function testTransferFailure_0(address from, uint256 value) public {
+    function testTransferFailure_0(address to, uint256 value) public {
         kevm.infiniteGas();
         ERC20 erc20 = new ERC20("Bucharest Workshop Token", "BWT");
         kevm.symbolicStorage(address(erc20));
         vm.startPrank(address(0));
         vm.expectRevert("ERC20: transfer from the zero address");
-        erc20.transfer(from, value);
+        erc20.transfer(to, value);
     }
 
     function testTransferFailure_1(uint256 value) public {
@@ -101,46 +101,25 @@ contract ERC20Test is Test, KEVMCheats {
         kevm.infiniteGas();
         vm.assume(alice != address(0));
         vm.assume(bob != address(0));
-        bytes32 storageIndexA = getStorageLocationForKey(alice, BALANCES_STORAGE_INDEX);
         ERC20 erc20 = new ERC20("Bucharest Workshop Token", "BWT");
         kevm.symbolicStorage(address(erc20));
-        vm.assume(uint256(vm.load(address(erc20), storageIndexA)) < amount);
+        vm.assume(erc20.balanceOf(alice) < amount);
         vm.startPrank(alice);
         vm.expectRevert("ERC20: transfer amount exceeds balance");
         erc20.transfer(bob, amount);
     }
 
-    function testTransferSuccess() public {
-        address alice = address (1231231231);
-        address bob = address(23111111);
-        uint256 amount = 200;
-        uint256 balanceA = 400;
-        uint256 balanceB = 400;
-        bytes32 storageIndexA = getStorageLocationForKey(alice, BALANCES_STORAGE_INDEX);
-        bytes32 storageIndexB = getStorageLocationForKey(bob  , BALANCES_STORAGE_INDEX);
+    function testTransferSuccess_1(address alice, uint256 amount, uint256 balanceA) public {
+        kevm.infiniteGas();
+        vm.assume(alice != address(0));
+        vm.assume(amount <= balanceA);
         ERC20 erc20 = new ERC20("Bucharest Workshop Token", "BWT");
-        vm.assume(uint256(vm.load(address(erc20), storageIndexA)) == balanceA);
-        vm.assume(uint256(vm.load(address(erc20), storageIndexB)) == balanceB);
-        vm.startPrank(address(alice));
-        erc20.transfer(bob, amount);
-        assert(uint256(vm.load(address(erc20), storageIndexA)) == balanceA - amount);
-        assert(uint256(vm.load(address(erc20), storageIndexB)) == balanceB + amount);
+        kevm.symbolicStorage(address(erc20));
+        vm.assume(erc20.balanceOf(alice) == balanceA);
+        vm.startPrank(alice);
+        erc20.transfer(alice, amount);
+        assert(erc20.balanceOf(alice) == balanceA);
     }
-
-    // function testTransferSuccess(address alice, address bob, uint256 balanceA, uint256 balanceB, uint256 amount) public {
-    //     bytes32 storageIndexA = getStorageLocationForKey(alice, BALANCES_STORAGE_INDEX);
-    //     bytes32 storageIndexB = getStorageLocationForKey(bob  , BALANCES_STORAGE_INDEX);
-    //     ERC20 erc20 = new ERC20("Bucharest Workshop Token", "BWT");
-    //     vm.assume(uint256(vm.load(address(erc20), storageIndexA)) == balanceA);
-    //     vm.assume(uint256(vm.load(address(erc20), storageIndexB)) == balanceB);
-    //     vm.assume(amount <= balanceA);
-    //     vm.assume(alice != address(0));
-    //     vm.assume(bob != address(0));
-    //     vm.startPrank(address(alice));
-    //     erc20.transfer(alice, amount);
-    //     assert(uint256(vm.load(address(erc20), storageIndexA)) == balanceA - amount);
-    //     assert(uint256(vm.load(address(erc20), storageIndexB)) == balanceB + amount);
-    // }
 
 }
 
