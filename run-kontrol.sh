ed815568aa5eeaf9d1fb3b3522650b3f4b9f3984
rv-jenkins <devops@runtimeverification.com>
2024-03-29 12:46:47 -0600

    Updating ReADME documentation, run-kontrol script formatting and steps to follow the README example more closely. Adding some media files for Example showing

--- run-kontrol.sh
+++ run-kontrol.sh
@@ -1,54 +1,78 @@
 #!/usr/bin/env bash
-set -euxo pipefail
+set -uxo pipefail
 
+#####################
+# Sameple Functions #
+#####################
+verbose=""
 kontrol_build() {
-    kontrol build --require lemmas.k --module-import ERC20:DEMO-LEMMAS \
-            "${verbose}"
+    kontrol build "$@"
 }
 
 kontrol_prove() {
-    kontrol prove "${verbose}"\
-            "${break_on_calls}"\
-            "${bug_report}"\
-            "$@"
+    kontrol prove "$@"
 }
 
-# kontrol_show() {
-#     kontrol show "${verbose}" "${test}" "$@"
-# }
+kontrol_show() {
+    kontrol show "${test}" "$@"
+}
 
-# kontrol_to_dot() {
-#     kontrol to-dot "${verbose}" "${test}" "$@"
-# }
+kontrol_to_dot() {
+    kontrol to-dot "${test}" "$@"
+}
 
-# kontrol_view() {
-#     kontrol view-kcfg "${verbose}" "${test}" "$@"
-# }
+kontrol_view() {
+    kontrol view-kcfg "${test}" "$@"
+}
 
-# kontrol_list() {
-#     kontrol list "${verbose}" "$@"
-# }
+kontrol_list() {
+    kontrol list "$@"
+}
 
-# kontrol_remove_node() {
-#     node_id="$1" ; shift
-#     kontrol remove-node "${verbose}" "${test}" ${node_id} "$@"
-# }
+kontrol_remove_node() {
+    node_id="$1" ; shift
+    kontrol remove-node "${test}" ${node_id} "$@"
+}
 
-# kontrol_simplify_node() {
-#     node_id="$1" ; shift
-#     kontrol simplify-node "${verbose}" "${test}" ${node_id} "${bug_report}" "$@"
-# }
+kontrol_simplify_node() {
+    node_id="$1" ; shift
+    kontrol simplify-node "${test}" ${node_id} "${bug_report}" "$@"
+}
 
-# kontrol_step_node() {
-#     node_id="$1" ; shift
-#     kontrol step-node "${verbose}" "${test}" ${node_id} "${bug_report}" "$@"
-# }
+kontrol_step_node() {
+    node_id="$1" ; shift
+    kontrol step-node "${test}" ${node_id} "${bug_report}" "$@"
+}
 
-# kontrol_section_edge() {
-#     edge_id="$1" ; shift
-#     kontrol section-edge "${verbose}" "${test}" ${edge_id} "${bug_report}" "$@"
-# }
+kontrol_section_edge() {
+    edge_id="$1" ; shift
+    kontrol section-edge "${test}" ${edge_id} "${bug_report}" "$@"
+}
 
+#################################
+# Kore RPC Server Stuck? Run .. #
+#################################
+# pkill kore-rpc || true
+
+kontrol_build --require lemmas.k --module-import ERC20:DEMO-LEMMAS --verbose
+kontrol_list
+kontrol_prove -j8 \
+            --verbose \
+            --bug-report=BUGREPORT.bug \
+            --match-test Examples.test_assert_bool_failing
+            # --match-test Examples.test_assert_bool_passing\
+            # --match-test Examples.test_wmul_increasing_overflow\
+            # --match-test Examples.test_wmul_increasing\
+            # --match-test Examples.test_wmul_increasing_positive\
+            # --match-test Examples.test_wmul_increasing_gt_one\
+            # --match-test Examples.test_wmul_weakly_increasing_positive\
+            # --match-test Examples.test_wmul_wdiv_inverse_underflow\
+            # --match-test Examples.test_wmul_wdiv_inverse
+kontrol_list --verbose
+
+###########################
+# Additional Tests to Run #
+###########################
 #test=Examples.test_assert_bool_failing
 #test=Examples.test_assert_bool_passing
 #test=Examples.test_wmul_increasing_overflow
@@ -59,30 +83,43 @@ kontrol_prove() {
 #test=Examples.test_wmul_wdiv_inverse_underflow
 #test=Examples.test_wmul_wdiv_inverse
 
-verbose=
-verbose=--verbose
+###################################
+# Review Assert bool Failing Test #
+###################################
+test=Examples.test_assert_bool_failing
+kontrol_show --verbose
 
-break_on_calls=--break-on-calls
-break_on_calls=
+################
+# Modify Nodes #
+################
+kontrol_remove_node 4b6c47..d6d6d3
+kontrol_prove --reinit
 
-bug_report="--bug-report=BUGREPORT.bug"
-# bug_report=
+##############################
+# Additional Reinit Examples #
+##############################
+# test=AssertTest.test_sum_10
+# date
+# kontrol_prove --reinit
+# date
+# kontrol_prove --reinit
+# date
+# test=AssertTest.test_sum_100
+# date
+# kontrol_prove --reinit
+# date
+# kontrol_prove --reinit
+# date
+# test=AssertTest.test_sum_1000
+# date
+# kontrol_prove --reinit
+# date
+# kontrol_prove --reinit
+# date
 
-# Uncomment these lines as needed
-# pkill kore-rpc || true
-kontrol_build --rekompile --regen
-# kontrol_list
-# kontrol_remove_node 4b6c47..d6d6d3
-kontrol_prove -j9                                              \
-    --match-test Examples.test_assert_bool_failing             \
-    --match-test Examples.test_assert_bool_passing             \
-    --match-test Examples.test_wmul_increasing_overflow        \
-    --match-test Examples.test_wmul_increasing                 \
-    --match-test Examples.test_wmul_increasing_positive        \
-    --match-test Examples.test_wmul_increasing_gt_one          \
-    --match-test Examples.test_wmul_weakly_increasing_positive \
-    --match-test Examples.test_wmul_wdiv_inverse_underflow     \
-    --match-test Examples.test_wmul_wdiv_inverse
+##########################
+# Additional Run Example #
+##########################
 # kontrol_prove --reinit
 # kontrol_section_edge --sections 4 d38e0e..ee4ec8,593be8..e93e52
 # kontrol_show # --no-minimize --node 35880c..e4389e --node 17e757..6c2e55
@@ -90,24 +127,3 @@ kontrol_prove -j9                                              \
 # for test in ERC20Test.testAllowance ERC20Test.testApproveFailure_0 ERC20Test.testApproveFailure_1 ERC20Test.testApproveSuccess ERC20Test.testBalanceOf ERC20Test.testDecimals ERC20Test.testNameAndSymbol ERC20Test.testTotalSupply ERC20Test.testTransferFailure_0 ERC20Test.testTransferFailure_1 ERC20Test.testTransferFailure_2 ERC20Test.testTransferFromFailure ERC20Test.testTransferFromSuccess_0 ERC20Test.testTransferFromSuccess_1 ERC20Test.testTransferSuccess_0 ERC20Test.testTransferSuccess_1; do
 #     kontrol_to_dot
 # done
-
-# test=AssertTest.test_sum_10
-# date
-# kontrol_prove --reinit
-# date
-# kontrol_prove --reinit
-# date
-# 
-# test=AssertTest.test_sum_100
-# date
-# kontrol_prove --reinit
-# date
-# kontrol_prove --reinit
-# date
-# 
-# test=AssertTest.test_sum_1000
-# date
-# kontrol_prove --reinit
-# date
-# kontrol_prove --reinit
-# date
