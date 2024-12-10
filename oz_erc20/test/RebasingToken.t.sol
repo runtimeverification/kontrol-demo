074353579d6ff900eb72589092559fa99dc14b21
palinatolmach <polina.tolmach@gmail.com>
2024-12-10 16:30:55 +0400

    Add `RebasingToken` test

---
+++ oz_erc20/test/RebasingToken.t.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+
+contract RebasingTokenTest is Test {
+    function check_balanceToRebasingCredits(uint256 rebasingCreditsPerToken, uint256 balance) public {
+        vm.assume(rebasingCreditsPerToken <= 1e27 && rebasingCreditsPerToken >= 1e18);
+        vm.assume(balance <= 1e25);
+
+        uint256 rebasingCredits = ((balance) * rebasingCreditsPerToken + 1e18 - 1) / 1e18;
+        uint256 actualBalance = (rebasingCredits * 1e18) / rebasingCreditsPerToken;
+        assert(actualBalance == balance);
+    }
+}
